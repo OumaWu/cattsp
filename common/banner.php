9b412c06fca0827bd254b5d0a9b1292903de7af7
OumaWu <1114110754@qq.com>
2018-06-11 10:13:49 +0800

    update .gitignore

--- common/banner.php
+++ common/banner.php
@@ -2,6 +2,6 @@
     <div class="header-tool clearfix">
         <h1 style="font-size:40px; color:#0072E3; margin-top:20px;"><b>欢迎来到到中国-东盟太阳能技术转移平台</b></h1>
         <h1 style="font-size:30px !important; color: #0072E3; margin-top:20px;"><b>Welcome to China-ASEAN Solar
-                Technology Transfer Online Platform</b></h1>
+                Technology Transfer Service Platform</b></h1>
     </div>
 </div>
