4bfc78e275779a660f50e45af3f23836ecbf236b
OumaWu <1114110754@qq.com>
2018-06-20 11:00:35 +0800

    优化页面结构

--- common/navbar.php
+++ common/navbar.php
@@ -3,10 +3,10 @@
     <div class="w1220">
         <ul id="nav" class="nav clearfix">
             <li class="nLi cur on" id="home">
-                <h3 style="width: 200px; text-align: center;"><a href="home.php" target="_self">首页</a></h3>
+                <h3 style="width: 100px; text-align: center;"><a href="home.php" target="_self">首页</a></h3>
             </li>
             <li class="nLi" id="solartech">
-                <h3><a href="solartech.php" target="_blank">太阳能技术</a></h3>
+                <h3><a href="solartech.php" target="_blank">成果与专利</a></h3>
             </li>
             <li class="nLi" id="demands">
                 <h3><a href="demands.php" target="_blank">企业需求</a></h3>
@@ -14,11 +14,17 @@
             <li class="nLi" id="specialists">
                 <h3><a href="specialists.php" target="_blank">专家咨询</a></h3>
             </li>
+            <li class="nLi" id="mall">
+                <h3><a href="#" target="_blank">科技商城</a></h3>
+            </li>
             <li class="nLi" id="information">
                 <h3><a href="information.php?category_id=2" target="_blank">资讯大厅</a></h3>
             </li>
             <li class="nLi" id="notification">
                 <h3><a href="information.php?category_id=1" target="_blank">通知公告</a></h3>
+            </li>
+            <li class="nLi" id="policies">
+                <h3><a href="#" target="_blank">政策法规</a></h3>
             </li>
             <li class="nLi" id="aboutus">
                 <h3><a href="aboutus.php" target="_blank">关于我们</a></h3>
