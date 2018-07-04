9ac32764dd3db27c910db67d54d5a0c7d5a96ae8
OumaWu <1114110754@qq.com>
2018-07-05 01:35:39 +0800

    修改图片上传功能，将上传至服务器的图片文件名加密

--- sql/deleteTech.php
+++ sql/deleteTech.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-define('FILE_UPLOAD_PATH', "../user_files/");
+define('FILE_UPLOAD_PATH', "../user_files");
 include("connection.php");
 $url = "../mytech.php";
 $id = $_GET["id"];
@@ -17,31 +17,35 @@ if (!empty($id) && !empty($user_id)) {
         //先执行语句删除数据库中的条目
         if ($result->execute()) {
 
-            $dir = FILE_UPLOAD_PATH . $user_id ."/solartech/" . $id;
+            $pdo->commit();
+            echo "<script> alert('删除成功！！');</script>";
+            echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
 
-            //然后删除用户对应的图片文件目录
-
-            if(is_dir($dir)) {
-                $files = array_diff(scandir($dir), array('.','..'));
-
-                foreach ($files as $file) {
-                    unlink("$dir/$file");
-                }
+//            $dir = FILE_UPLOAD_PATH;
+//
+//            //然后删除用户对应的图片文件目录
+//
+//            if(is_dir($dir)) {
+//                $files = array_diff(scandir($dir), array('.','..'));
+//
+//                foreach ($files as $file) {
+//                    unlink("$dir/$file");
+//                }
 
                 //删除成功则提交事务
-                if (rmdir($dir)) {
-                    $pdo->commit();
-                    echo "<script> alert('删除成功！！');</script>";
-                    echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
-                }
-
-                //删除失败则回滚
-                else {
-                    $pdo->rollBack();
-                    echo "<script> alert('删除失败！！');</script>";
-                    echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
-                }
-            }
+//                if (rmdir($dir)) {
+//                    $pdo->commit();
+//                    echo "<script> alert('删除成功！！');</script>";
+//                    echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
+//                }
+//
+//                //删除失败则回滚
+//                else {
+//                    $pdo->rollBack();
+//                    echo "<script> alert('删除失败！！');</script>";
+//                    echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
+//                }
+//            }
 
         } else {
             echo "<script> alert('删除失败！！');</script>";
--- sql/insertTech.php
+++ sql/insertTech.php
@@ -14,84 +14,122 @@ if (!empty($_POST["title"]) && !empty($_POST["description"]) && !empty($_POST["l
     $userid = $_SESSION["userid"];
     $user = $_SESSION["user"];
 
-    $images = array();
-
-    //接收图片
-    foreach ($_FILES as $file) {
-        //生成基于图片内容的加密图片名
-        $img = hash_file('md5', $file["tmp_name"]);
-        //将图片名放入数组中
-        array_push($images, $img);
-    }
-
-    $sql = "INSERT INTO `solar_technologies` (`id`, `title`, `entreprise`, `location`, `email`, `description`, `date`, `image1`, `image2`, `image3`, `image4`, `image5`, `userid`)"
-        ." VALUES (NULL, '$title', '$entreprise', '$location', '$email', '$description', now(), '$images[0]', '$images[1]', '$images[2]', '$images[3]', '$images[4]', '$userid')";
+    $sql = "INSERT INTO `solar_technologies` (`id`, `title`, `entreprise`, `location`, `email`, `description`, `date`, `userid`)"
+        . " VALUES (NULL, '$title', '$entreprise', '$location', '$email', '$description', now(), '$userid')";
 
     try {
+
         $pdo->beginTransaction();
         $result = $pdo->prepare($sql);
         //执行插入语句
         if ($result->execute()) {
 
+            //获取新插入的技术成果id
+            $last_id = $pdo->lastInsertId();
 
-            $pdo->commit();
-            echo "<script> alert('发布太阳能技术成功！！');</script>";
-            echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
+            //储存文件名的数组
+            $images = array();
 
-//            //获取新插入的技术成果id
-//            $last_id = $pdo->lastInsertId();
-//
-//            //如果上传图片成功，则提交事务
-//            if(uploadImg($userid, $last_id)) {
-//                $pdo->commit();
-//                echo "<script> alert('发布太阳能技术成功！！');</script>";
-//                echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
-//            }
-//            //上传失败则回滚
-//            else {
-//                $pdo->rollBack();
-//                echo "<script> alert('发布太阳能技术失败！！');</script>";
-//                echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url2\">";
-//            }
+            //如果上传图片成功，则修改数据库图片名
+            if (uploadImg($user, $last_id,$images)) {
+
+//                foreach ($images as $image) {
+//                    echo $image . "<br>";
+//                }
+
+                //改名成功，则提交事务
+                if (modifyImgName($pdo, $images, $last_id)) {
+                    $pdo->commit();
+                    echo "<script> alert('发布太阳能技术成功！！');</script>";
+                    echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
+                }
+                //改名失败则回滚
+                else {
+                    $pdo->rollBack();
+                    echo "<script> alert('发布太阳能技术失败！！');</script>";
+                  echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url2\">";
+                }
+            }
+            //上传失败则回滚
+            else {
+                $pdo->rollBack();
+                echo "<script> alert('发布太阳能技术失败！！');</script>";
+                  echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url2\">";
+            }
 
         } else {
             echo "<script> alert('发布太阳能技术失败！！');</script>";
             echo $pdo->errorInfo();
-//            echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url2\">";
+            echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url2\">";
         }
 
     } catch (PDOException $e) {
         die("错误!!: " . $e->getMessage() . "<br>");
     }
+
 } else {
     echo "<script> alert('请填必填信息！！');</script>";
     echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url2\">";
 }
 
+function modifyImgName($pdo, $images, $last_id)
+{
+    $sql = "UPDATE `solar_technologies`"
+        . " SET `image1` = '$images[0]', `image2` = '$images[1]', `image3` = '$images[2]', `image4` = '$images[3]', `image5` = '$images[4]'"
+        . "WHERE `solar_technologies`.`id` = $last_id";
 
-function uploadImg($userid, $last_id) {
+    try {
+        $result = $pdo->prepare($sql);
+        //执行插入语句
+        if ($result->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (PDOException $e) {
+        die("错误!!: " . $e->getMessage() . "<br>");
+    }
+}
+
+function uploadImg($user, $last_id, &$images)
+{
 
     $success = false;
     $path = FILE_UPLOAD_PATH;
 
     //如果文件夹不存在则创建它
-    if (!file_exists($path)){
-        mkdir ($path,0777,true);
+    if (!file_exists($path)) {
+        mkdir($path, 0777, true);
     }
 
     // 给图片名前添加上传路径，并上传
     foreach ($_FILES as $file) {
         //如果文件名不为空，则加上上传路径
-        if($file['name'] != null) {
+        if ($file['name'] != null) {
 
             $file_path = $path . $file['name'];
 
             //如果图片不存在，则上传
             if (!file_exists($file_path)) {
                 if (move_uploaded_file($file["tmp_name"], $file_path)) {
+
+                    //获取后缀名
+                    $img_ext = substr($file['name'], strrpos($file['name'], '.'));
+
+                    //根据图片内容生成一个哈希字串
+                    $img_hash_name = hash_file('md5', $file_path) . $img_ext;
+
+                    //用生成的哈希字串和用户名以及技术成果id生成一个新的加密文件名
+                    $img_hash_name =  $user . $last_id . $img_hash_name;
+
+                    //将上传的图片改名为加密文件名
+                    rename($file_path,$path .$img_hash_name);
+
+                    //将生成的加密文件名存至数组
+                    array_push($images, $img_hash_name);
+
                     $success = true;
-                }
-                else {
+                } else {
                     $success = false;
                 }
             }
