fe88b8253f5862ace15aaf2c812820124eca59a1
OumaWu <1114110754@qq.com>
2018-07-04 11:15:19 +0800

    企业用户删除技术成果专利时移除存放图片的文件夹功能完成

--- sql/deleteTech.php
+++ sql/deleteTech.php
@@ -1,30 +1,60 @@
 <?php
+session_start();
+define('FILE_UPLOAD_PATH', "../user_files/");
 include("connection.php");
-
 $url = "../mytech.php";
 $id = $_GET["id"];
+$user_id = $_SESSION['userid'];
 
 /* 删除数据sql */
-if (!empty($id)) {
+if (!empty($id) && !empty($user_id)) {
     try {
         $sql = "delete from `solar_technologies` where `id` like $id";
 
         $pdo->beginTransaction();
         $result = $pdo->prepare($sql);
+
+        //先执行语句删除数据库中的条目
         if ($result->execute()) {
-            echo "<script> alert('删除成功！！');</script>";
-            echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
+
+            $dir = FILE_UPLOAD_PATH . $user_id ."/solartech/" . $id;
+
+            //然后删除用户对应的
+            if(is_dir($dir)) {
+                $files = array_diff(scandir($dir), array('.','..'));
+
+                foreach ($files as $file) {
+                    unlink("$dir/$file");
+                }
+
+                //删除成功则提交事务
+                if (rmdir($dir)) {
+                    $pdo->commit();
+                    echo "<script> alert('删除成功！！');</script>";
+                    echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
+                }
+
+                //删除失败则回滚
+                else {
+                    $pdo->rollBack();
+                    echo "<script> alert('删除失败！！');</script>";
+//                    echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
+                }
+            }
+
         } else {
             echo "<script> alert('删除失败！！');</script>";
-            echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
+            echo $pdo->errorInfo();
+//            echo "<meta http-equiv=\"refresh\" content=\"0.5;url=$url\">";
         }
-        $pdo->commit();
+
     } catch (PDOException $e) {
         die("Error!!: " . $e->getMessage() . "<br>");
-        $pdo->rollBack();
     }
 } else {
-    echo "没有接收到ID！！！";
+    echo "没有接收到ID或用户ID为空！！！" . "<br>";
+    echo "user id : " . $user_id . "<br>";
+    echo "tech id : " . $id . "<br>";
 }
 
 ?>
--- test.php
+++ test.php
@@ -1,39 +1,55 @@
 <?php
 
 //定义图片上传路径
-//define('FILE_UPLOAD_PATH', $_SERVER['DOCUMENT_ROOT'] . "/cattsp/user_files/");
-define('FILE_UPLOAD_PATH', "./user_files/solartech/testUpload/");
+define('FILE_UPLOAD_PATH', "./user_files/solartech/testUpload");
 
-if (!empty($_POST)) {
+$dir = FILE_UPLOAD_PATH;
 
-    // 从$_FILES数组里提取图片信息
-    $image = [
-        (string)$_FILES['img1']['name'],
-        (string)$_FILES['img2']['name'],
-        (string)$_FILES['img3']['name'],
-        (string)$_FILES['img4']['name'],
-        (string)$_FILES['img5']['name']
-    ];
+if(is_dir($dir)) {
+    $files = array_diff(scandir($dir), array('.','..'));
 
-    // 给图片名前添加上传路径，并上传
-    foreach ($_FILES as $file) {
-        //如果文件名不为空，则加上上传路径
-        if($file['name'] != null) {
-            $file_path = FILE_UPLOAD_PATH . $file['name'];
-
-            echo $file_path . "<br>";
-
-            //如果图片不存在，则上传
-            if (!file_exists($file_path)) {
-                if (move_uploaded_file($file["tmp_name"], $file_path)) {
-                    echo "图片".$file_path."上传成功！！". "<br>";
-                }
-            }
-        }
+    foreach ($files as $file) {
+        unlink("$dir/$file");
     }
 
-
-
+    if (rmdir($dir)) {
+        echo "删除文件夹成功！！";
+    }
 }
+
+
+
+
+//if (!empty($_POST)) {
+//
+//    // 从$_FILES数组里提取图片信息
+//    $image = [
+//        (string)$_FILES['img1']['name'],
+//        (string)$_FILES['img2']['name'],
+//        (string)$_FILES['img3']['name'],
+//        (string)$_FILES['img4']['name'],
+//        (string)$_FILES['img5']['name']
+//    ];
+//
+//    // 给图片名前添加上传路径，并上传
+//    foreach ($_FILES as $file) {
+//        //如果文件名不为空，则加上上传路径
+//        if($file['name'] != null) {
+//            $file_path = FILE_UPLOAD_PATH . $file['name'];
+//
+//            echo $file_path . "<br>";
+//
+//            //如果图片不存在，则上传
+//            if (!file_exists($file_path)) {
+//                if (move_uploaded_file($file["tmp_name"], $file_path)) {
+//                    echo "图片".$file_path."上传成功！！". "<br>";
+//                }
+//            }
+//        }
+//    }
+//
+//
+//
+//}
 
 ?>
